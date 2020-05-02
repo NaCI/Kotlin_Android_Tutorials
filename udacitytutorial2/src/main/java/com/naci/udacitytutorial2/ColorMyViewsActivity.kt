35c480c50be82f9fa896e98990cbcf5faf84b785
nozyildirim <nozyildirim@tmobtech.com>
2020-05-02 19:22:38 +0300

    Complete color my views tutorial

--- udacitytutorial2/src/main/java/com/naci/udacitytutorial2/ColorMyViewsActivity.kt
+++ udacitytutorial2/src/main/java/com/naci/udacitytutorial2/ColorMyViewsActivity.kt
@@ -1,7 +1,10 @@
 package com.naci.udacitytutorial2
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_color_my_views.*
 
 class ColorMyViewsActivity : AppCompatActivity() {
 
@@ -9,5 +12,32 @@ class ColorMyViewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_color_my_views)
 
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(
+                boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootLayout,
+                redButton, yellowButton, greenButton
+            )
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when(view.id) {
+            R.id.boxOneText -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.boxTwoText -> view.setBackgroundColor(Color.GRAY)
+            R.id.boxThreeText -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.boxFourText -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.boxFiveText -> view.setBackgroundResource(android.R.color.holo_blue_bright)
+            R.id.redButton -> boxThreeText.setBackgroundResource(R.color.red)
+            R.id.yellowButton -> boxFourText.setBackgroundResource(R.color.yellow)
+            R.id.greenButton -> boxFiveText.setBackgroundResource(R.color.green)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
