e32a4cb5d8f5e6fc6d4553c87486a89f1ab47c16
nozyildirim <nozyildirim@tmobtech.com>
2020-05-03 03:02:05 +0300

    Add simple navigation tutorial

---
+++ udacitytutorial3/src/main/java/com/naci/udacitytutorial3/TitleFragment.kt
@@ -0,0 +1,38 @@
+package com.naci.udacitytutorial3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.naci.udacitytutorial3.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container, false
+        )
+        /*
+        binding.playButton.setOnClickListener { view: View ->
+//            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment) // Kotlin extention function
+        }
+        */
+        // Different representation of it
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+        return binding.root
+    }
+
+}
