738e29ade47d78a1d2235a2ca3a5ae0daa131b64
nozyildirim <nozyildirim@tmobtech.com>
2020-05-03 18:12:32 +0300

    Add actionbar up navigation && Add menu

--- udacitytutorial3/src/main/java/com/naci/udacitytutorial3/TitleFragment.kt
+++ udacitytutorial3/src/main/java/com/naci/udacitytutorial3/TitleFragment.kt
@@ -1,13 +1,12 @@
 package com.naci.udacitytutorial3
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.naci.udacitytutorial3.databinding.FragmentTitleBinding
 
 /**
@@ -32,7 +31,18 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         )
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
 }
