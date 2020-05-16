2337778aac52ad74cffaf9e56a460aa2494864cc
Naci <naciozyildirim@gmail.com>
2020-05-16 16:50:02 +0300

    Add viewmodel and livedata on udacity tutorial 5

---
+++ udacitytutorial5/src/main/java/com/example/udacitytutorial5/screens/game/GameViewModel.kt
@@ -0,0 +1,83 @@
+package com.example.udacitytutorial5.screens.game
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import timber.log.Timber
+
+class GameViewModel : ViewModel() {
+
+    // The current word
+    var word = MutableLiveData<String>("")
+
+    // The current score
+    var score = MutableLiveData<Int>()
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    init {
+        Timber.i("GameViewModel created!")
+        resetList()
+        nextWord()
+        score.value = 0
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+//            gameFinished()
+        } else {
+            word.value = wordList.removeAt(0)
+        }
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        score.value = score.value?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score.value = score.value?.plus(1)
+        nextWord()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Timber.i("GameViewModel destroyed!")
+    }
+}
