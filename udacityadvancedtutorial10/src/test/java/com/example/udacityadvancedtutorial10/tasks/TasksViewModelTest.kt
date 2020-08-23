4ba01b4b24f074e1c407f251173abfd07d5d0e5a
Naci Özyıldırım <naci.ozyildirim@multinet.com.tr>
2020-08-23 17:22:22 +0300

    Add ViewModel and LiveData tests

---
+++ udacityadvancedtutorial10/src/test/java/com/example/udacityadvancedtutorial10/tasks/TasksViewModelTest.kt
@@ -0,0 +1,51 @@
+package com.example.udacityadvancedtutorial10.tasks
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.udacityadvancedtutorial10.getOrAwaitValue
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest {
+
+    @get:Rule
+    var instantTestExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var tasksViewModel: TasksViewModel
+
+    // Tüm testlerde ortak kullanılacak değişkenler için böyle bir tanım yapılabilir
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
+    @Test
+    fun addNewTask_setsNewTaskEvent() {
+        // Given a fresh ViewModel from @Before annotation
+
+        // When adding a new task
+        tasksViewModel.addNewTask()
+
+        // Then the new task event is triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+        // Given a fresh ViewModel from @Before annotation
+
+        // When the filter type is ALL_TASKS
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        // Then the "Add task" action is visible
+        val value = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
+        assertThat(value, `is`(true))
+    }
+}
