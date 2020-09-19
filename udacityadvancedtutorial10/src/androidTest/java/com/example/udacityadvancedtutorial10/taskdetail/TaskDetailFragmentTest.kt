6f7b1ffebb8cac5b631d8ed93ea6008076d6900a
Naci Özyıldırım <naci.ozyildirim@multinet.com.tr>
2020-09-19 13:50:14 +0300

    Add fragment tests to udacityAdvancedTutorial10

--- udacityadvancedtutorial10/src/androidTest/java/com/example/udacityadvancedtutorial10/taskdetail/TaskDetailFragmentTest.kt
+++ udacityadvancedtutorial10/src/androidTest/java/com/example/udacityadvancedtutorial10/taskdetail/TaskDetailFragmentTest.kt
@@ -1,6 +1,9 @@
 package com.example.udacityadvancedtutorial10.taskdetail
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.example.udacityadvancedtutorial10.R
@@ -10,6 +13,7 @@ import com.example.udacityadvancedtutorial10.data.source.FakeAndroidTestReposito
 import com.example.udacityadvancedtutorial10.data.source.TasksRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.core.IsNot.not
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -43,5 +47,37 @@ class TaskDetailFragmentTest {
         val bundle = TaskDetailFragmentArgs(activeTest.id).toBundle()
         launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
         Thread.sleep(2000)
+
+        // THEN - Task details are displayed on the screen
+        // make sure that the title/description are both shown and correct
+        onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("Active Task")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("Task Description Wulululu")))
+        // and make sure the "active" checkbox is shown unchecked
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(not(isChecked())))
+    }
+
+    @Test
+    fun completedTaskDetails_DisplayedInUi() = runBlockingTest {
+        // GIVEN - Add completed task to the DB
+        val completedTask = Task("Completed Task", "Brush your feet", true)
+        repository.saveTask(completedTask)
+
+        // WHEN - Details fragment launched to display task
+        val bundle = TaskDetailFragmentArgs(completedTask.id).toBundle()
+        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+        Thread.sleep(1000)
+
+        // THEN - Task details are displayed on the screen
+        // make sure that the title/description are both shown and correct
+        onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("Completed Task")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("Brush your feet")))
+        // and make sure the "active" checkbox is shown checked
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isChecked()))
     }
 }
