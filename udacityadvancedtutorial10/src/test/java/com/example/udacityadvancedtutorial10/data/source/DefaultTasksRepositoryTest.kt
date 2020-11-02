a88abb50736f63e8cc26f0c16154f9a46be8fc7b
Naci Özyıldırım <naci.ozyildirim@multinet.com.tr>
2020-11-02 19:59:02 +0300

    Add testcoroutinescope rule to repository test

--- udacityadvancedtutorial10/src/test/java/com/example/udacityadvancedtutorial10/data/source/DefaultTasksRepositoryTest.kt
+++ udacityadvancedtutorial10/src/test/java/com/example/udacityadvancedtutorial10/data/source/DefaultTasksRepositoryTest.kt
@@ -1,5 +1,6 @@
 package com.example.udacityadvancedtutorial10.data.source
 
+import com.example.udacityadvancedtutorial10.MainCoroutineRule
 import com.example.udacityadvancedtutorial10.data.Result
 import com.example.udacityadvancedtutorial10.data.Task
 import kotlinx.coroutines.Dispatchers
@@ -8,9 +9,15 @@ import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.core.IsEqual
 import org.junit.Assert.assertThat
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class DefaultTasksRepositoryTest {
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
     private val task1 = Task("Title1", "Description1")
     private val task2 = Task("Title2", "Description2")
     private val task3 = Task("Title3", "Description3")
@@ -29,16 +36,20 @@ class DefaultTasksRepositoryTest {
         tasksLocalDataSource = FakeDataSource(localTasks.toMutableList())
 
         tasksRepository = DefaultTasksRepository(
-            // TODO Dispatchers.Unconfined should be replaced with Dispatchers.Main
-            //  this requires understanding more about coroutines + testing
-            //  so we will keep this as Unconfined for now.
-            tasksRemoteDataSource, tasksLocalDataSource, Dispatchers.Unconfined
+            tasksRemoteDataSource, tasksLocalDataSource, Dispatchers.Main
         )
     }
 
+    /*
+     * Generally, only create one TestCoroutineDispatcher to run a test.
+     * Whenever you call runBlockingTest, it will create a new TestCoroutineDispatcher if you don't specify one.
+     * MainCoroutineRule includes a TestCoroutineDispatcher.
+     * So, to ensure that you don't accidentally create multiple instances of TestCoroutineDispatcher,
+     * use the mainCoroutineRule.runBlockingTest instead of just runBlockingTest.
+     */
     @ExperimentalCoroutinesApi
     @Test
-    fun getTasks_requestsAllTasksFromRemoteDataSource() = runBlockingTest {
+    fun getTasks_requestsAllTasksFromRemoteDataSource() = mainCoroutineRule.runBlockingTest {
         // WHEN tasks are requested from the tasks repository
         val tasks = tasksRepository.getTasks(true) as Result.Success
 
